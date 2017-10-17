2772715fbe54843d17433b25570213039e34dbf7
Rubens Correa <rubens.correa20@gmail.com>
2017-10-17 16:11:28 -0200

    Adicionando o front end com angular e adicionando arquivo cors.js e modificando no server.js

---
+++ angular1/app/billingCycle/billingCycleController.js
@@ -0,0 +1,54 @@
+(function() {
+	angular.module('primeiraApp').controller('BillingCycleCtrl', [
+		'$http',
+		'msgs',
+		'tabs',
+		BillingCycleController
+	])
+
+	function BillingCycleController($http, msgs, tabs) {
+		const vm = this
+		const url = 'http://localhost:3003/api/billingCycles' 
+
+
+		vm.refresh = function() {
+			$http.get(url).then(function(response) {
+				vm.billingCycle = {}
+				vm.billingCycles = response.data
+				tabs.show(vm, {tabList: true, tabCreate: true})
+			})
+		}
+
+		vm.create = function() {
+			
+ 			$http.post(url, vm.billingCycle).then(function(response) {
+ 				vm.refresh()
+				msgs.addSuccess('Operacao realizada com sucesso!')
+			}).catch(function(response) {
+				msgs.addError(response.data.errors)
+			})
+		}
+
+		vm.showTabUpdate = function(billingCycle) {
+			vm.billingCycle = billingCycle
+			tabs.show(vm, {tabUpdate: true})
+		}
+
+		vm.showTabDelete = function(billingCycle) {
+			vm.billingCycle = billingCycle
+			tabs.show(vm, {tabDelete: true})
+		}
+
+		vm.delete = function() {
+			const deleteUrl = `${url}/${vm.billingCycle._id}`
+			$http.delete(deleteUrl, vm.billingCycle).then(function(response){
+				vm.refresh()
+				msgs.addSuccess('Operacao realizada com sucesso')
+			}).catch(function(response) {
+				msgs.addError(response.data.errors)
+			})
+		}
+
+		vm.refresh()
+	}
+})()
---
+++ angular1/app/config/routes.js
@@ -0,0 +1,18 @@
+(function() {
+angular.module('primeiraApp').config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider.state('dashboard', {
+			url: "/dashboard",
+			templateUrl: "dashboard/dashboard.html"
+		}).state('billingCycle', {
+			url: "/billingCycle",
+			templateUrl: "billingCycle/tabs.html"
+		})
+
+		$urlRouterProvider.otherwise('/dashboard')
+	}
+])
+
+})()
